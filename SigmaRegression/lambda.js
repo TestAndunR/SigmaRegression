288364772c430b10fa3548ff1b981e484d430a3f
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-11 14:37:23 +0530

    Remove cloud watch trail and add Kinesis as a resource

--- SigmaRegression/lambda.js
+++ SigmaRegression/lambda.js
@@ -1,30 +1,26 @@
 let AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
 const ses = new AWS.SES();
 exports.handler = function (event, context, callback) {
+	let para = event.para;
+	let key = event.key;
 	// let subject = event.subject;
 	// let body = event.body;
 	console.log(event);
-	// ses.sendEmail({
-	// 	Destination: {
-	// 		ToAddresses: ['andun@adroitlogic.com'],
-	// 		// CcAddresses: [],
-	// 		// BccAddresses: []
-	// 	},
-	// 	Message: {
-	// 		Subject: {
-	// 			Data: subject
-	// 		},
-	// 		Body: {
-	// 			Text:{
-	// 				Data: ""
-	// 			}
-	// 		}
-	// 	},
-	// 	Source: 'kannangarar1993@gmail.com',
-	// }, function (err, data) {
-	// 	if (err) console.log(err, err.stack); // an error occurred
-	// 	else console.log(data);           // successful response
-	// });
+
+	kinesis.putRecord({
+		Data: para,
+		PartitionKey: key,
+		StreamName: 'Kinesis-data-resource'
+	}).promise()
+		.then(data => {
+			// your logic goes here
+			console.log(data);
+		})
+		.catch(err => {
+			console.log(err);
+			// error handling goes here
+		});
 
 
 	callback(null, 'Successfully executed');
