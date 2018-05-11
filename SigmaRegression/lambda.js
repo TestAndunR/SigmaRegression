9c12626dbd6a885208905c8867d437120c5d489a
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-11 10:04:49 +0530

    Add ses

--- SigmaRegression/lambda.js
+++ SigmaRegression/lambda.js
@@ -1,6 +1,31 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ses = new AWS.SES();
+exports.handler = function (event, context, callback) {
+	let subject = event.subject;
+	let body = event.body;
+	
+	ses.sendEmail({
+		Destination: {
+			ToAddresses: ['andun@adroitlogic.com'],
+			CcAddresses: [],
+			BccAddresses: []
+		},
+		Message: {
+			Body: {
+				Text: {
+					Data: body
+				}
+			},
+			Subject: {
+				Data: subject
+			}
+		},
+		Source: 'kannangarar1993@gmail.com',
+	}, function (err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else console.log(data);           // successful response
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
