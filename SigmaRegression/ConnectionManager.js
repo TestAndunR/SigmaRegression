9181a173fc64ed5d8db70dbb5f86745edebf65a8
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-14 10:57:08 +0530

    include a RDS

---
+++ SigmaRegression/ConnectionManager.js
@@ -0,0 +1,12 @@
+module.exports=function(){
+
+    this.dbConnections = [];
+
+    this.dbConnections["andun123"] = {
+            host: "andun123.czndmdv7dw6d.us-east-1.rds.amazonaws.com",
+            port: "6000",
+            user: "Andun",
+            password: process.env.Password_rdsAndun123,
+            database: "TestRDS",
+        };
+    };
--- SigmaRegression/lambda.js
+++ SigmaRegression/lambda.js
@@ -1,11 +1,19 @@
 let AWS = require('aws-sdk');
+let connectionManager = require('./ConnectionManager');
+let SL_AWS = require('slappforge-sdk-aws');
+const rds = new SL_AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
-	
-	let r = event.radius;
 
-	let area = pi*r*r;
+	console.log(process.env.PORT)
 
-	console.log(area);
+	// You must always end/destroy the DB connection after it's used
+	rds.beginTransaction({
+		instanceIdentifier: 'andun123'
+	}, function (error, connection) {
+		if (error) { throw err; }
+	});
+
+
 
 	callback(null, 'Successfully executed');
 }
