bd2adf5845fcd7a78e9dd8a4b41c4feb7b27b3df
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-14 10:00:01 +0530

    area of a circle with environment variable

--- SigmaRegression/lambda.js
+++ SigmaRegression/lambda.js
@@ -1,30 +1,11 @@
 let AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
-const ses = new AWS.SES();
 exports.handler = function (event, context, callback) {
-	let para = event.para;
-	let key = event.key;
-	// let subject = event.subject;
-	// let body = event.body;
-	kinesis.putRecord({
-		Data: para,
-		PartitionKey: key,
-		StreamName: 'Kinesis-data-resource'
-	}).promise()
-		.then(data => {
-			// your logic goes here
-			console.log(data);
-		})
-		.catch(err => {
-			console.log(err);
-			// error handling goes here
-		});
-
-
-
-	console.log(event);
-
 	
+	let r = event.radius;
+
+	let area = pi*r*r;
+
+	console.log(area);
 
 	callback(null, 'Successfully executed');
 }
