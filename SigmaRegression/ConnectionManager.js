ca3f0ab802c036dc60e3682c5cbd133c7ff95f25
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-14 14:20:18 +0530

    remove RDS and Add DDB

--- SigmaRegression/ConnectionManager.js
+++ SigmaRegression/ConnectionManager.js
@@ -3,10 +3,10 @@ module.exports=function(){
     this.dbConnections = [];
 
     this.dbConnections["andun123"] = {
-            host: "andun123.czndmdv7dw6d.us-east-1.rds.amazonaws.com",
-            port: "6000",
-            user: "Andun",
-            password: process.env.Password_rdsAndun123,
-            database: "TestRDS",
+             host: "andun123.czndmdv7dw6d.us-east-1.rds.amazonaws.com",
+             port: "6000",
+             user: "Andun",
+             password: process.env.Password_rdsAndun123,
+             database: "TestRDS",
         };
     };
--- SigmaRegression/lambda.js
+++ SigmaRegression/lambda.js
@@ -1,17 +1,36 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 let connectionManager = require('./ConnectionManager');
 let SL_AWS = require('slappforge-sdk-aws');
 const rds = new SL_AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
-	console.log(process.env.PORT)
+	console.log(process.env)
 
 	// You must always end/destroy the DB connection after it's used
-	rds.beginTransaction({
-		instanceIdentifier: 'andun123'
-	}, function (error, connection) {
-		if (error) { throw err; }
+	// rds.beginTransaction({
+	// 	instanceIdentifier: 'andun123'
+	// }, function (error, connection) {
+	// 	if (error) { throw err; }
+	// 	connection.end();
+
+	// });
+	ddb.query({
+		TableName: 'TestDynamo',
+		ExpressionAttributeValues: {},
+		KeyConditionExpression: 'SongTitle = :t',
+		FilterExpression: '',
+	}, function (err, data) {
+		if (err) {
+			//handle error
+			console.log("error");
+		} else {
+			//your logic goes here
+			console.log("Success");
+		}
 	});
+
+
 
 
 
