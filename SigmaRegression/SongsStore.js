16e3ccb88a3ad2acac5ce7ffd946b71026bf1e63
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-14 15:09:38 +0530

    New lambda new ddb

--- SigmaRegression/SongsStore.js
+++ SigmaRegression/SongsStore.js
@@ -1,13 +1,19 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
+	let Id = event.id;
 	let song = event.song;
 	let source = event.source;
 	let artist = event.artist;
 
 	ddb.put({
 		TableName: 'TestDynamo',
-		Item: { 'Song': 'song', 'Source': 'source', 'Artist': 'artist' }
+		Item: {
+			'Song': song,
+			'Source': source,
+			'Artist': artist,
+			'Id': Id
+		}
 	}, function (err, data) {
 		if (err) {
 			console.log(err);
